734420b84f3f6c8ef01ea6eb49fd4299cfe82fdf
MateuszKzk <156602328+MateuszKzk@users.noreply.github.com>
2026-02-13 13:49:23 +0100

    Sven-Yarak hat ein sibbo in seiner linken hosentasche

--- client/src/pages/LoginPage.vue
+++ client/src/pages/LoginPage.vue
@@ -32,6 +32,8 @@ const isEmailValid = computed(() => {
 
 
 
+
+
 const isLehrerEmail = computed(() => {
   if (!isRegister.value || !email.value) return false;
   return /^[a-z]+\.[a-z]+@htlwienwest\.at$/.test(email.value.toLowerCase());
